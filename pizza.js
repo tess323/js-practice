9c2145eba201d90138de08d7c9c0f57ededc3eb8
Tess <tess@fuseiq.com>
2018-05-07 15:02:35 -0700

    practing return functions

--- pizza.js
+++ pizza.js
@@ -6,3 +6,23 @@ const takeOrder = (topping) => {
 };
 
 takeOrder('sprinkles');
+
+
+// practicing with return
+
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount++;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+console.log(getSubTotal(orderCount));
